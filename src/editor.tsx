eeee594e2b03172f1fd8893ba51a150814e4833e
sylvrs <sylvrs@proton.me>
2024-08-04 03:57:34 -0500

    fix: ensure that updating the signal propagates to the editor

--- src/editor.tsx
+++ src/editor.tsx
@@ -2,6 +2,7 @@ import { createSignal, createEffect, onCleanup, JSX, onMount, mergeProps } from 
 import { editor as Editor } from "monaco-editor";
 import loader, { Monaco } from "@monaco-editor/loader";
 
+
 export interface MonacoEditorProps {
     language?: string
     value?: string
@@ -14,53 +15,48 @@ export interface MonacoEditorProps {
     options?: Editor.IStandaloneEditorConstructionOptions
     saveViewState?: boolean
     onChange?: (value: string, event: Editor.IModelContentChangedEvent) => void
-    onMount?: (monaco: Monaco, editor: Editor.IStandaloneCodeEditor) => void
-    onBeforeUnmount?: (monaco: Monaco, editor: Editor.IStandaloneCodeEditor) => void
-  }
+};
 
-  const HoneyTheme: Editor.IStandaloneThemeData = {
-    base: "vs-dark",
-    inherit: true,
-    rules: [],
-    colors: {
-      "editor.background": "#0A101B",
-    },
-  };
-  
-  export const MonacoEditor = (inputProps: MonacoEditorProps) => {
+const HoneyTheme: Editor.IStandaloneThemeData = {
+  base: "vs-dark",
+  inherit: true,
+  rules: [],
+  colors: { "editor.background": "#0A101B" },
+};
+
+export const MonacoEditor = (inputProps: MonacoEditorProps) => {
     const props = mergeProps(
       {
-        theme: 'vs-dark',
-        width: '100%',
-        height: '100%',
+        theme: "vs-dark",
+        width: "100%",
+        height: "100%",
         saveViewState: true,
       },
       inputProps,
-    )
+    );
+    let containerRef!: HTMLDivElement;
   
-    let containerRef!: HTMLDivElement
+    const [monaco, setMonaco] = createSignal<Monaco>();
+    const [editor, setEditor] = createSignal<Editor.IStandaloneCodeEditor>();
   
-    const [monaco, setMonaco] = createSignal<Monaco>()
-    const [editor, setEditor] = createSignal<Editor.IStandaloneCodeEditor>()
-  
-    let abortInitialization: (() => void) | undefined = undefined;
+    let abortInitialization: (() => void) | undefined;
+    let monacoOnChangeSubscription: { dispose: () => void } | undefined;
+    let isOnChangeSuppressed = false;
   
     onMount(async () => {
       const loadMonaco = loader.init();
       abortInitialization = () => loadMonaco.cancel();
+  
       try {
-        const monaco = await loadMonaco
-        const editor = monaco.editor.create(
-          containerRef,
-          {
+        const monaco = await loadMonaco;
+        const editor = monaco.editor.create(containerRef, {
             automaticLayout: true,
             ...props.options,
           },
           props.overrideServices,
         );
-        setMonaco(monaco)
-        setEditor(editor)
-        props.onMount?.(monaco, editor)
+        setMonaco(monaco);
+        setEditor(editor);
   
         monaco.editor.defineTheme("honey", HoneyTheme);
         monaco.editor.setTheme("honey");
@@ -68,10 +64,12 @@ export interface MonacoEditorProps {
         monaco.editor.setModelLanguage(editor.getModel()!, props.language ?? "text");
 
         editor.setValue(props.value ?? "");
+  
+        monacoOnChangeSubscription = editor.onDidChangeModelContent(event => {
+          if (!isOnChangeSuppressed) props.onChange?.(editor.getValue(), event);
+        });
       } catch (error: any) {
-        if (error?.type === "cancelation") {
-          return
-        }
+        if (error?.type === "cancelation") return;
         console.error("Could not initialize Monaco: ", error);
       }
     });
@@ -80,20 +78,36 @@ export interface MonacoEditorProps {
       const _editor = editor();
       if (!_editor) {
         abortInitialization?.();
-        return;
+        return
       }
   
-      props.onBeforeUnmount?.(monaco()!, _editor);
+      monacoOnChangeSubscription?.dispose();
       _editor.getModel()?.dispose();
       _editor.dispose();
-    })
+    });
   
     createEffect(() => {
       const _editor = editor();
-      if (!_editor || typeof props.value === "undefined" || !_editor.getOption(monaco()!.editor.EditorOption.readOnly)) {
+      if (!_editor || typeof props.value === "undefined") {
         return;
       }
-      _editor.setValue(props.value);
+      if (_editor.getOption(monaco()!.editor.EditorOption.readOnly)) {
+        _editor.setValue(props.value);
+        return;
+      }
+
+      if (props.value !== _editor.getValue()) {
+        isOnChangeSuppressed = true;
+        _editor.executeEdits("", [
+          {
+            range: _editor.getModel()!.getFullModelRange(),
+            text: props.value,
+            forceMoveMarkers: true,
+          },
+        ]);
+        _editor.pushUndoStop();
+        isOnChangeSuppressed = false;
+      }
     });
   
     return (
